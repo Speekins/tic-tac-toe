c0a434b6d0c1c9c2ee0d443c1b835168703d849f
Spencer Haka <spencer.haka@gmail.com>
2023-03-21 07:52:30 -0600

    Initial commit

---
+++ src/classes/player.ts
@@ -0,0 +1,14 @@
+class Player {
+  name: string
+  score: string
+  constructor(name: string) {
+    this.name = name,
+    this.score = ''
+  }
+
+  incrementScore(position: string): void {
+    this.score += position
+  }
+}
+
+module.exports = Player
